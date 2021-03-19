fb140c4e46b86b96b3b5e1df3a3c51fd9adb3c75
Moon Beob Sik <tjswl756@naver.com>
2021-03-19 13:52:59 +0900

    feat: add code insert, select usertbl, buytbl

--- mysql workbench/sqldbAndTable.sql
+++ mysql workbench/sqldbAndTable.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS sqldb;
 create DATABASE sqldb;
 use sqldb;
 CREATE TABLE usertbl
@@ -19,3 +20,29 @@ CREATE TABLE usertbl
   amount	SMALLINT NOT NULL,
   FOREIGN KEY (userID) REFERENCES usertbl(userID)
  );
+ 
+ INSERT INTO usertbl VALUES('LSG', '이승기', 1987, '서울', '011', '1111111', 182, '2008-8-8');
+INSERT INTO usertbl VALUES('KBS', '김범수', 1979, '경남', '011', '2222222', 173, '2012-4-4');
+INSERT INTO usertbl VALUES('KHH', '김경호', 1971, '전남', '019', '3333333', 177, '2007-7-7');
+INSERT INTO usertbl VALUES('JYP', '조용필', 1950, '경기', '011', '4444444', 173, '2009-4-4');
+INSERT INTO usertbl VALUES('SSK', '성시경', 1979, '서울', NULL, NULL, 186, '2013-12-12');
+INSERT INTO usertbl VALUES('LJB', '임재범', 1963, '서울', '016', '6666666', 182, '2009-9-9');
+INSERT INTO usertbl VALUES('YJS', '윤종신', 1969, '경남', NULL, NULL, 170, '2005-5-5');
+INSERT INTO usertbl VALUES('EJW', '은지원', 1972, '경북', '011', '8888888', 174, '2014-3-3');
+INSERT INTO usertbl VALUES('JKW', '조관우', 1965, '경기', '018', '6666666', 172, '2010-10-10');
+INSERT INTO usertbl VALUES('BBK', '바비킴', 1973, '서울', '010', '0000000', 186, '2013-5-5');
+INSERT INTO buytbl VALUES(NULL, 'KBS', '운동화', NULL, 30, 2);
+INSERT INTO buytbl VALUES(NULL, 'KBS', '노트북', '전자', 1000, 1);
+INSERT INTO buytbl VALUES(NULL, 'JYP', '모니터', '전자', 200, 1);
+INSERT INTO buytbl VALUES(NULL, 'BBK', '모니터', '전자', 200, 5);
+INSERT INTO buytbl VALUES(NULL, 'KBS', '청바지', '의류', 50, 3);
+INSERT INTO buytbl VALUES(NULL, 'BBK', '메모리', '전자', 80, 10);
+INSERT INTO buytbl VALUES(NULL, 'SSK', '책', '서적', 15, 5);
+INSERT INTO buytbl VALUES(NULL, 'EJW', '책', '서적', 15, 2);
+INSERT INTO buytbl VALUES(NULL, 'EJW', '청바지', '의류', 50, 1);
+INSERT INTO buytbl VALUES(NULL, 'BBK', '운동화', NULL, 30, 2);
+INSERT INTO buytbl VALUES(NULL, 'EJW', '책', '서적', 15, 1);
+INSERT INTO buytbl VALUES(NULL, 'BBK', '운동화', NULL, 30, 2);
+
+SELECT * FROM usertbl;
+SELECT * FROM buytbl;
